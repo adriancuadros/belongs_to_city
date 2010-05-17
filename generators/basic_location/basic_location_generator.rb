14403fe1d532647014993bac4ee71c783cf71fde
Adrian Cuadros <adrian@innku.com>
2010-05-17 01:17:03 -0500

    Deletes files with old gem name

--- generators/basic_location/basic_location_generator.rb
+++
@@ -1,7 +0,0 @@
-class BasicLocationGenerator < Rails::Generator::Base
-  def manifest
-    record do |m|
-      m.migration_template "migrate/create_basic_location_tables.rb", "db/migrate", :migration_file_name => "create_basic_location_tables"
-    end
-  end
-end
--- generators/basic_location/templates/models/state.rb
+++
@@ -1,3 +0,0 @@
-class Country < ActiveRecord::Base
-  belongs_to      :country
-end
